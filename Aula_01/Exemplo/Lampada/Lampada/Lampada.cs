4e9c9a6db5e65fe07f3aecf3921231511791f2d0
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2024-02-23 12:38:57 -0300

    Add files via upload

---
+++ Aula_01/Exemplo/Lampada/Lampada/Lampada.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada
+{
+    public class Lampada
+    {
+        public bool Ligada { get; set; }
+        public string? Cor { get; set; }
+        public int Potencia { get; set; }
+    }
+}
---
+++ Aula_01/Exemplo/Lampada/Lampada/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            // INSTÂNCIA DE UM OBJETO
+            Lampada lampada1 = new Lampada();
+            Lampada lampada2 = new Lampada();
+
+            //ACESSANDO UM ATRIBUTO PUBLICO DO OBJETO
+            lampada1.Ligada = true;
+            lampada1.Potencia = 150;
+            lampada1.Cor = "Vermelho";
+
+            lampada2.Ligada = false;
+            lampada2.Potencia = 100;
+            lampada2.Cor = "Verde";
+
+            //VISUALIZANDO OS VALORES DE ATRIBUTOS
+            Console.WriteLine("***************************************");
+            Console.WriteLine("ESSA É A LÂMPADA 1");
+            Console.WriteLine("A lâmpada está Ligada? " + lampada1.Ligada);
+            Console.WriteLine("A Potência da Lâmpada é: " + lampada1.Potencia);
+            Console.WriteLine("A cor da Lâmpada é: " + lampada1.Cor);
+            Console.WriteLine("***************************************");
+            Console.WriteLine("ESSA É A LÂMPADA 2");
+            Console.WriteLine("A lâmpada está Ligada? " + lampada2.Ligada);
+            Console.WriteLine("A Potência da Lâmpada é: " + lampada2.Potencia);
+            Console.WriteLine("A cor da Lâmpada é: " + lampada2.Cor);
+            Console.WriteLine("***************************************");
+        }
+    }
+}
