b79cab92f938ad07e74b8e5720bb2f0bf50b4d43
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2024-02-29 12:35:22 -0300

    Add files via upload

---
+++ Aula_02/Exemplo/Lampada/Lampada/Lampada.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada
+{
+    public class Lampada
+    {
+        private bool Ligada { get; set; }
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
+
+        public void ligar()
+        {
+            Ligada = true;
+        }
+
+        public void desligar()
+        {
+            Ligada = false;
+        }
+
+        public bool estaLigada()
+        {
+            return Ligada;        
+        }
+
+        public void maisPotencia()
+        {
+            Potencia = +12;
+        }
+
+        public void menosPotencia()
+        {
+            Potencia = -12;
+        }
+
+        public int qualPotencia()
+        {
+            return Potencia;
+        }
+
+        public void vermelho()
+        {
+            Cor = "Vermelho";
+        }
+
+        public void verde()
+        {
+            Cor = "Verde";
+        }
+
+        public void azul()
+        {
+            Cor = "Azul";
+        }
+
+        public string qualCor()
+        {
+            return Cor;
+        }
+    }
+}
---
+++ Aula_02/Exemplo/Lampada/Lampada/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando um novo objeto da classe lâmpada (nova instancia)
+            Lampada lamp1 = new Lampada();
+
+            //Chamando o método pra ligar a lamp1
+            lamp1.ligar();
+            lamp1.maisPotencia();
+            lamp1.azul();
+
+            //Chamando o método para verificar se lamp1 está ligada
+            Console.WriteLine("A Lâmpada está ligada? " + lamp1.estaLigada());
+            Console.WriteLine("A Potencia da lâmpada é de " + lamp1.qualPotencia());
+            Console.WriteLine("A Cor da Lâmpada é: " + lamp1.qualCor());
+
+
+
+        }
+    }
+}
