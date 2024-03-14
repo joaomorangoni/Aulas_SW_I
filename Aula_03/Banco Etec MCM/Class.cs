db97791ebb468cf21ff41f096c1cad46601c8d5c
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2024-03-14 12:33:40 -0300

    Add files via upload

---
+++ Aula_03/Banco Etec MCM/Class.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    public class Conta
+    {
+        private string? Num_conta { get; set; }
+
+        private float Saldo { get; set; }
+
+        private float Limite { get; set; }
+
+        private float Saldo_Limite { get; set; }
+
+        public float consulta_saldo()
+        {
+            return Saldo;
+        }
+
+        public void deposito(float valor_deposito)
+        {
+            Saldo += valor_deposito;
+            Console.WriteLine("Agora seu saldo é de R$ " + consulta_saldo());
+        }
+
+        public void sacar(float valor_saque)
+        {
+            if(valor_saque > Saldo_Limite)
+            {
+                Console.WriteLine("VOCÊ NÃO TEM SALDO SUFICIENTE!");
+            }
+            else
+            {
+                Saldo -= valor_saque;
+                Console.WriteLine("Agora seu saldo é de R$ " + consulta_saldo());
+            }
+        }
+
+        public void ajustar_limite(float valor_limite)
+        {
+            Limite = valor_limite;
+            Console.WriteLine("Agora seu limite é de R$ " + Limite);
+            Saldo_Limite = Limite + Saldo;
+            Console.WriteLine("Seu saldo + limite é de R$ " + Saldo_Limite);
+        }
+    }
+}
---
+++ Aula_03/Banco Etec MCM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace ContaBancaria
+{
+    internal class Program
+    { 
+        static void Main(string[] args) 
+        {
+        Console.WriteLine("BANCO ETEC MCM");
+
+        Conta conta = new Conta();
+        Console.WriteLine("Seu saldo inicial é de: R$ " + conta.consulta_saldo());
+
+            conta.deposito(500);
+
+            conta.sacar(1000);
+
+            Console.WriteLine("Seu saldo inicial é de: R$ " + conta.consulta_saldo());
+
+            conta.ajustar_limite(500);
+        }
+
+
+    }
+}
