bc303c207ca056a6089daff690f2abb4a42e7460
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2024-03-21 10:35:56 -0300

    Add files via upload

---
+++ Aula_04/Avião/Avião/Aviao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aviao
+{
+    public class Aviao
+    {
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
+        public int Velocidade { get; set; }
+        public int Altitude { get; set; }
+
+        public void Subir(int m)
+        {
+            Altitude += m;
+        }
+
+        public void Descer(int m)
+        {
+            if (Altitude >= 0)
+            {
+                Altitude -= m;
+            }
+            else
+            {
+                Altitude = 0;
+            }
+        }
+
+        public void Acelerar(int v)
+        {
+            Velocidade += v;
+        }
+
+        public void Reduzir(int v)
+        {
+            if (Velocidade >= 0)
+            {
+                Velocidade -= v;
+            }
+            else
+            {
+                Velocidade = 0;
+            }
+        }
+    }
+}
---
+++ Aula_04/Avião/Avião/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aviao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aviao aviao = new Aviao();
+
+            aviao.Marca = "Gol";
+            aviao.Modelo = "737-700";
+
+            aviao.Acelerar(10);
+            aviao.Subir(220);
+
+            Console.WriteLine($"A altura do avião é: {aviao.Altitude}m, A velocidade é: {aviao.Velocidade}km/h, A marca é: {aviao.Marca}, e o modelo é: {aviao.Modelo}");
+        }
+    }
+}
