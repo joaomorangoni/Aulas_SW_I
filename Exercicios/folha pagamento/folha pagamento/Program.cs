910ecc088e80861b658089fb918601f1174364ba
João Pedro Morangoni <124791845+joaomorangoni@users.noreply.github.com>
2024-04-12 11:42:47 -0300

    Add files via upload

---
+++ Exercicios/folha pagamento/folha pagamento/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace folha_pagamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pagamento p = new Pagamento();
+            p.Nome = "João";
+            p.Sal = 8700;
+            p.HED = 1;
+            p.HEN = 4;
+            p.ND = 0;
+            p.Fal = 47;
+            p.DE = 5;
+            p.REF = 8;
+            p.Val = 1;
+            Console.WriteLine(p.FolhaDePagamento());
+        }
+    }
+}
---
+++ Exercicios/folha pagamento/folha pagamento/pagamento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace folha_pagamento
+{
+    class Pagamento
+    {
+        public string Nome { get; set; }
+        public float Sal { get; set; }
+        public float HED { get; set; }
+        public float HEN { get; set; }
+        public int ND { get; set; }
+        public float Fal { get; set; }
+        public float DE { get; set; }
+        public float REF { get; set; }
+        public float Val { get; set; }
+
+        public string FolhaDePagamento()
+        {
+            double HoraExtra = HED * Sal / 160 + HEN * 1.2 * Sal / 160;
+
+            double INAMPS = 0.08 * Sal;
+
+            double SalF = ND * 0.05;
+
+            double SalB = Sal + HoraExtra + SalF;
+
+            double faltas = Fal * Sal / 160;
+
+            double imposto_de_renda = 0.08 * Sal;
+
+            return $"-----------------------------\nNome: {Nome}\n-----------------------------\nSalário: {Sal}\nHoras Extras: {HoraExtra}\nSalário Família: {SalF}\nSalário Mínimo: {SalB}\nINAMPS: {INAMPS}\nFaltas: {faltas}\nRefeições: {REF}\nVales: {Val}\nDescontos Eventuais: {DE}\n-----------------------------\nImposto de renda: {imposto_de_renda}\nSalário Líquido: {SalB - INAMPS - faltas - REF - Val - DE - imposto_de_renda}";
+        }
+
+
+    }
+}
